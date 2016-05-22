8a8c42e6c8769f82c1c9f0720e573f89e7442c92
Brandon Telle <brandon.telle@gmail.com>
2016-05-22 11:43:33 -0700

    Change test.py to run all tests

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
 '''
 Script for testing out the response for any given request
 '''
+
 from __future__ import print_function
 from lambda_function import lambda_handler
 import json
 import sys
+import glob
+import os
 from argparse import ArgumentParser
 
+class UnexpectedResponseException(Exception):
+	pass
+
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-i', '--input_json', required=True)
-    args = parser.parse_args()
-    request_obj = json.load(open(args.input_json))
-    print ('Request JSON')
-    print (json.dumps(request_obj, indent=2))
-    response = lambda_handler(request_obj)
-    print ('Response JSON')
-    print (json.dumps(response, indent=2))
+	for f in glob.glob('../alexa-boardgame-answers/test-data/*.json'):
+		print (os.path.basename(f))
+		request_obj = json.load(open(f))
+		print ('Request JSON')
+		print (json.dumps(request_obj, indent=2))
+		response = lambda_handler(request_obj)
+		print ('Response JSON')
+		print (json.dumps(response, indent=2))
+
+		if response['response']['outputSpeech']['text'] in ["I'm sorry, I don't know the answer.", "I couldn't understand your question, try again."]:
+			raise UnexpectedResponseException()
+		
+		print("\n\n")
     
